5c90d9d92d6bf52ca944f7c4e98e77c6cdcb3e53
deva0209 <sgnandev@gmail.com>
2022-12-18 21:54:00 +0530

    second commit

--- Abc.java
+++ Abc.java
@@ -2,5 +2,6 @@ class Abc {
     public static void main(String[] args) {
         System.out.println("Welcome to Java!");
         system.out.println("Welcome all"); 
+        system.out.println("Welcome Github"); 
     }
 }
